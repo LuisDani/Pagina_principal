cda2ce2760ff9c70aefd32598fff3d101e3b091e
LuisDani <spartandanii117@gmail.com>
2023-12-01 00:06:35 -0600

    Parch 1.1.0 API 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.pagina_principal"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pagina_principal"
